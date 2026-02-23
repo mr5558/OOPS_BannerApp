327f0aceaee94737f5267db65cdb06dff5747a30
mr5558 <mr5558@srmist.edu.in>
2026-02-23 21:51:59 +0530

    UC4

---
+++ UC4.java
@@ -0,0 +1,21 @@
+public class UC4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+            " *****   *****   *****    ***** ",
+            "*     * *     * *     *  *     *",
+            "*     * *     * *     *  *      ",
+            "*     * *     * *****    *****  ",
+            "*     * *     * *             * ",
+            "*     * *     * *       *     * ",
+            " *****   *****  *        *****  "
+
+        };
+
+        for (int i = 0; i < banner.length; i++) {
+            System.out.println(banner[i]);
+        }
+    }
+}
