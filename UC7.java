a113b0bea6045f56ac9f018317c05720f8777dc8
mr5558 <mr5558@srmist.edu.in>
2026-03-02 11:13:32 +0530

    UC7

---
+++ UC7.java
@@ -0,0 +1,57 @@
+class CharacterPattern {
+    char character;
+    String[] pattern; // 7-line pattern
+
+    CharacterPattern(char character, String[] pattern) {
+        this.character = character;
+        this.pattern = pattern;
+    }
+
+    public String[] getPattern() {
+        return pattern;
+    }
+}
+
+public class UC7 {
+    public static void main(String[] args) {
+
+        CharacterPattern O = new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        CharacterPattern P = new CharacterPattern('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        CharacterPattern S = new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+
+        CharacterPattern[] word = {O, O, P, S};
+
+        for (int i = 0; i < 7; i++) {
+            for (CharacterPattern cp : word) {
+                System.out.print(cp.getPattern()[i] + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
